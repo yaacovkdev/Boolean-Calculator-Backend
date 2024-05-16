d77a2890b69619549a07a0566822c513440a6ce3
yaacovkdev <yaacov.kochatkov@gmail.com>
2024-05-16 01:37:45 -0700

    input field on client

--- boolean-calculator-client/src/components/SubmitForm.jsx
+++ boolean-calculator-client/src/components/SubmitForm.jsx
@@ -1,14 +1,30 @@
+import { useRef } from "react";
 import "./SubmitForm.scss";
 
 function SubmitForm(props) {
+  const inputFormula = useRef();
+
+  const submitFormula = async () => {
+    //post the formula to the api
+  };
+
   return (
     <div className="submit-form">
       <div className="submit-form__input">
-        <input id="boolean-input-field" placeholder="Write Boolean Formula" />
+        <div className="submit-form__input-field">
+          <input
+            id="boolean-input-field"
+            placeholder="Write Boolean Formula"
+            ref={inputFormula}
+          />
+          <button id="boolean-input-button" onClick={submitFormula}>
+            Post
+          </button>
+        </div>
+        <p id="text-tag">Finally Here!</p>
       </div>
-      <div className="submit-form__output">
-        dsadas
-      </div>
+
+      <div className="submit-form__output"></div>
     </div>
   );
 }
