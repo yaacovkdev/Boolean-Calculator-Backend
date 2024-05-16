6f628979ad238a2f1d3d9e472c0ea1dbdd5466b6
yaacovkdev <yaacov.kochatkov@gmail.com>
2024-05-15 17:15:21 -0700

    client

---
+++ boolean-calculator-client/src/components/SubmitForm.jsx
@@ -0,0 +1,16 @@
+import "./SubmitForm.scss";
+
+function SubmitForm(props) {
+  return (
+    <div className="submit-form">
+      <div className="submit-form__input">
+        <input id="boolean-input-field" placeholder="Write Boolean Formula" />
+      </div>
+      <div className="submit-form__output">
+        dsadas
+      </div>
+    </div>
+  );
+}
+
+export default SubmitForm;
