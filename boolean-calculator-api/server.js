b9aae776b8e54e0a1f513d3adb499f6107e53658
yaacovkdev <yaacov.kochatkov@gmail.com>
2024-05-23 18:16:56 -0700

    debugging

--- boolean-calculator-api/server.js
+++ boolean-calculator-api/server.js
@@ -1,7 +1,7 @@
-const path = require('path');
-const express = require('express');
-const cors = require('cors');
-const BooleanCalculator = require('../bcp-source/BooleanCalculator').default;
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const BooleanCalculator = require("../bcp-source/BooleanCalculator").default;
 
 const app = express();
 app.use(express.text());
@@ -10,14 +10,16 @@ app.use(cors());
 
 // app.use('/', express.static(path.join(__dirname, '/build')));
 
-app.post('/stuff', (req, res) => {
-  console.log(req.body);
-
+app.post("/stuff", (req, res) => {
   const calculation = BooleanCalculator(req.body);
-  
-  res.status(200).send(calculation);
+  // console.log(calculation);
+  if(calculation.length > 16 && typeof calculation != "string"){
+    res.status(400).send('way too large!');
+  } else {
+    res.status(200).send(calculation);
+  }
 });
 
-app.listen(8080, function() {
+app.listen(8080, function () {
   console.log("App started");
 });
